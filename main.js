fa99e33e6a1394c95e1fa037b4d5e27545d81027
cinxwei <cxinwei@softwaresecured.com>
2018-12-06 14:26:03 -0500

    - update version

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ const ArgumentParser = require('argparse').ArgumentParser;
 
 
 const parser = new ArgumentParser({
-    version: '1.1.5',
+    version: '1.1.6',
     addHelp:true,
     description: 'NPM security plugin'
 });
