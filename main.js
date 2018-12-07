287915a1b53d6d66b517730311d955f6ebc5df7c
cinxwei <cxinwei@softwaresecured.com>
2018-12-07 12:53:10 -0500

    - enable angular rule - update published version - regenerate jsdoc

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ const ArgumentParser = require('argparse').ArgumentParser;
 
 
 const parser = new ArgumentParser({
-    version: '1.1.7',
+    version: '1.1.71',
     addHelp:true,
     description: 'NPM security plugin'
 });
