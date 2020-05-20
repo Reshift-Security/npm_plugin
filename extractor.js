380a9c163b3ec8d7490cf904326ed257f52f7f75
qnguyen <qnguyen@softwaresecured.com>
2020-05-20 12:51:27 -0400

    Jira Title: OM-1335: prepare extracted files to be consumed by omega
    Description:
    renamed all .js files to .csv files within the output folder
    
    Tested it against project abbr-deasync and validated the output folder .js files get renamed to csv

--- extractor.js
+++ extractor.js
@@ -1,86 +1,129 @@
 'use strict';
 
+const assert = require('assert');
 const compressing = require('compressing');
 const fs = require('fs');
+const fsp = require('fs').promises;
 const java = require('java');
+const path = require('path');
+const promisify = require('util').promisify;
+const readdirp = promisify(fs.readdir);
+const statp = promisify(fs.stat);
 const tmp = require('tmp');
-const assert = require('assert');
-
 
 class Extractor {
-    constructor(projectRoot){
-        this.projectRoot = projectRoot;
+  constructor(projectRoot) {
+    this.projectRoot = projectRoot;
 
-        // FIXME: temporarily put the extractor JAR file within this folder.
-        // in the future the library will pull from Central Repository enabled by the node-java-maven
-        const depRoot = "./dependencies/";
-        this.dependencies = fs.readdirSync(depRoot)
-        this.dependencies.forEach(function(dependency){
-            java.classpath.push(depRoot + dependency);
-        })
+    // FIXME: temporarily put the extractor JAR file within this folder.
+    // in the future the library will pull from Central Repository enabled by the node-java-maven
+    const depRoot = './dependencies/';
+    this.dependencies = fs.readdirSync(depRoot);
+    this.dependencies.forEach(function (dependency) {
+      java.classpath.push(depRoot + dependency)
+    });
 
-        this.projectOutputDir = tmp.dirSync();
+    this.projectOutputDir = tmp.dirSync();
 
-        // set environment variables needed by the extractor jar
-        // CODEQL_EXTRACTOR_JAVASCRIPT_ROOT = where should the trap files go
-        // TRAP_FOLDER_JAVASCRIPT = where should the trap files go
-        // LGTM_SRC = point the extractor to the project root
-        // LGTM_INDEX_TYPESCRIPT = None to disable typescript parsing
-        process.env.CODEQL_EXTRACTOR_JAVASCRIPT_ROOT = this.projectOutputDir.name;
-        process.env.TRAP_FOLDER_JAVASCRIPT = this.projectOutputDir.name;
-        process.env.LGTM_SRC = this.projectRoot;
-        process.env.LGTM_INDEX_TYPESCRIPT = 'NONE';
+    // set environment variables needed by the extractor jar
+    // CODEQL_EXTRACTOR_JAVASCRIPT_ROOT = where should the trap files go
+    // TRAP_FOLDER_JAVASCRIPT = where should the trap files go
+    // LGTM_SRC = point the extractor to the project root
+    // LGTM_INDEX_TYPESCRIPT = None to disable typescript parsing
+    process.env.CODEQL_EXTRACTOR_JAVASCRIPT_ROOT = this.projectOutputDir.name;
+    process.env.TRAP_FOLDER_JAVASCRIPT = this.projectOutputDir.name;
+    process.env.LGTM_SRC = this.projectRoot;
+    process.env.LGTM_INDEX_TYPESCRIPT = 'NONE';
+  }
+
+  cleanup() {
+    if (this.projectOutputDir) {
+      this.projectOutputDir.removeCallback();
+      this.projectOutputDir = null;
     }
+    if (this.packagedFile) {
+      this.packagedFile.removeCallback();
+      this.packagedFile = null;
+    }
+  }
 
-    cleanup(){
-        if( this.projectOutputDir ){
-            this.projectOutputDir.removeCallback();
-            this.projectOutputDir = null;
+  /**
+   * The extractor works by pointing autobuild (from the extractor.jar) to a project folder by
+   * setting specific env variables and then running the extraction process.
+   * The output is a folder containing matching files to the project. These files are called "Trap" files.
+   */
+  doTrap() {
+    assert(this.projectOutputDir, 'invalid project output directory');
+    const extractorInstance = java.newInstanceSync(
+      'com.semmle.js.extractor.AutoBuild'
+    );
+    try {
+      java.callMethodSync(extractorInstance, 'run');
+    } catch (ex) {
+      // the Jar can return java.nio.file.NoSuchFileException: <tmpdir>/tools/data/externs
+      // this is normal
+      // console.log(ex.message);
+    }
+  }
+
+  async scan(directoryName, results = []) {
+    const files = await readdirp(directoryName);
+    for (const f of files) {
+      const fullPath = path.join(directoryName, f);
+      let stat = await statp(fullPath);
+      if (stat.isDirectory()) {
+        await this.scan(fullPath, results);
+      } else {
+        results.push(fullPath);
+      }
+    }
+    return results;
+  }
+
+  /**
+   * walks through all files within the output folder and converts .js files into .csv files (name change)
+   */
+  async renameTrap() {
+    assert(this.projectOutputDir, 'invalid project output directory');
+    await this.scan(this.projectOutputDir.name).then(function (files) {
+      for (const file of files) {
+        if (path.extname(file) == '.js') {
+          const newFile = file.replace(/(.*\.)js/, '$1csv');
+          fs.renameSync(file, newFile);
         }
-        if( this.packagedFile ){
-            this.packagedFile.removeCallback();
-            this.packagedFile = null;
-        }   
-    }
+      }
+    })
+  }
 
-    /**
-     * The extractor works by pointing autobuild (from the extractor.jar) to a project folder by
-     * setting specific env variables and then running the extraction process.
-     * The output is a folder containing matching files to the project. These files are called "Trap" files.
-     */
-    doTrap() {
-        assert(this.projectOutputDir, 'invalid project output directory');
-        const extractorInstance = java.newInstanceSync("com.semmle.js.extractor.AutoBuild");
-        try {
-            java.callMethodSync(extractorInstance, "run");
-        } catch(ex){
-            // the Jar can return java.nio.file.NoSuchFileException: <tmpdir>/tools/data/externs
-            // this is normal
-            // console.log(ex.message);
+  writeProjectMeta(meta) {
+    assert(this.projectOutputDir, 'invalid project output directory');
+
+    fs.writeFile(
+      this.projectOutputDir.name + '/projectMeta.json',
+      JSON.stringify(meta),
+      function (err) {
+
+        if (err) {
+          return console.log(err)
         }
-    }
+      }
+    );
+  }
 
-    writeProjectMeta(meta){
-        assert(this.projectOutputDir, 'invalid project output directory');
+  async compressOutput() {
+    this.packagedFile = tmp.fileSync();
+    await compressing.tar
+      .compressDir(this.projectOutputDir.name, this.packagedFile.name, {
+        ignoreBase: true
+      })
+      .catch(function (reason) {
+        console.error(reason)
+      });
 
-        fs.writeFile(this.projectOutputDir.name + '/projectMeta.json', JSON.stringify(meta), function (err) {
-            if (err) {
-                return console.log(err);
-            }
-        });
-    }
-
-    async compressOutput(){
-        this.packagedFile = tmp.fileSync();
-        await compressing.tar.compressDir(this.projectOutputDir.name, this.packagedFile.name, { ignoreBase: true })
-            .catch(function(reason){
-                console.error(reason);
-            });
-
-        return this.packagedFile.name;
-    }
+    return this.packagedFile.name;
+  }
 }
 
 module.exports = {
-    Extractor: Extractor
+  Extractor: Extractor
 }
--- main.js
+++ main.js
@@ -6,36 +6,39 @@ const extractor = require('./extractor.js');
 const git = require('./git.js');
 const transport = require('./transport.js');
 
-
-async function generateReport(configuration){
-    if( !configuration.isValid() ){
-        console.error("Invalid configuration used for the reshift-plugin-npm");
+async function generateReport(configuration) {
+    if (!configuration.isValid()) {
+        console.error('Invalid configuration used for the reshift-plugin-npm')
     }
 
     // lets get the git information for this project folder
-    const gitInstance = new git.Git(configuration.projectDir)
-    let meta = await gitInstance.branchInfo();
-    meta['commitHash'] = await gitInstance.commitHash();
+    const gitInstance = new git.Git(configuration.projectDir);
+    const meta = await gitInstance.branchInfo();
+    meta.commitHash = await gitInstance.commitHash();
 
-    var ex = new extractor.Extractor(configuration.projectDir)
+    var ex = new extractor.Extractor(configuration.projectDir);
     ex.writeProjectMeta(meta);
     ex.doTrap();
+    await ex.renameTrap();
     const compressedFile = await ex.compressOutput();
-    
+
     const sendMechanism = new transport.Transport();
-    sendMechanism.sendResult(configuration.token, configuration.endpoint, compressedFile);
+    sendMechanism.sendResult(
+        configuration.token,
+        configuration.endpoint,
+        compressedFile
+    );
     ex.cleanup();
 }
 
-(async() => {
-    if (typeof require != 'undefined' && require.main==module) {
+(async () => {
+    if (typeof require !== 'undefined' && require.main == module) {
         // const configuration = {
         //     'inDir': '<project root directory>',
         //     'token': '<token received during project onboarding with reshift-security>',
         //     'endpoint' : '<reshift endpoint - default to https://reshift.reshiftsecurity.com/'
         // };
-
         const configuration = new config.Config();
         await generateReport(configuration);
     }
-})();
+})()
